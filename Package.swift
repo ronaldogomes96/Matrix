0b02f9afdf1046fcd565ba36acaa374a4a4717e2
jessicaguiot <jessica.guiot97@gmail.com>
2021-07-30 10:31:28 -0300

    test11

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
         .target(
             name: "MatrixSwift",
             dependencies: ["Matrix"],
-            path: "MatrixCalculatorSwift"
+            path: "Matrix/MatrixCalculatorSwift"
         )
     ]
 )
